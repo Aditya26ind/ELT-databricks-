005419dbe5ec3c1f7539660979b6ab80814bde40
Aditya26ind <adi0699sal@gmail.com>
2026-06-05 16:04:12 +0530

    Cache spark session, add pandas to bundle dependencies

--- src/utils/session_spark.py
+++ src/utils/session_spark.py
@@ -3,17 +3,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+_spark = None
+
 
 def get_spark():
+    global _spark
+    if _spark is not None:
+        return _spark
+
     # ON_DATABRICKS set by run.py (serverless jobs don't set DATABRICKS_RUNTIME_VERSION)
     # DATABRICKS_RUNTIME_VERSION set by classic clusters
     if os.getenv("ON_DATABRICKS") or os.getenv("DATABRICKS_RUNTIME_VERSION"):
         from pyspark.sql import SparkSession
-        return SparkSession.builder.getOrCreate()
+        _spark = SparkSession.builder.getOrCreate()
+    else:
+        from databricks.connect import DatabricksSession
+        _spark = DatabricksSession.builder.remote(
+            host=os.getenv("databricks_host"),
+            token=os.getenv("databricks_token"),
+            serverless=True,
+        ).getOrCreate()
 
-    from databricks.connect import DatabricksSession
-    return DatabricksSession.builder.remote(
-        host=os.getenv("databricks_host"),
-        token=os.getenv("databricks_token"),
-        serverless=True,
-    ).getOrCreate()
+    return _spark
